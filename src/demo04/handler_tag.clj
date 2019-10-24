f8e9d249aaa9dbd006cb7a4e81bcfb61cc6e7ae0
fei.xiao <fei.xiao@yungoal.com>
2019-10-24 17:38:32 +0800

    show query count

--- src/demo04/handler_tag.clj
+++ src/demo04/handler_tag.clj
@@ -1,12 +1,14 @@
 (ns demo04.handler-tag
   (:require [monger.core :as mg]
             [monger.collection :as mc]
-            [monger.operators :refer [$each $addToSet $pull $in]]
+            [monger.query :refer :all]
+            [monger.operators :refer [$each $addToSet $pull $in $all]]
             [monger.conversion :refer [to-db-object from-db-object]]
             [ring.util.response :refer [response bad-request]])
   (:import (com.mongodb DuplicateKeyException)
            (org.bson.types ObjectId)))
 
+(def ^:private conn (mg/connect {:host "10.126.21.136"}))
 
 (defn convert-to-map [obj]
   (let [m (from-db-object obj true)]
@@ -16,8 +18,7 @@
       )))
 
 (defn handler [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
+  (let [db (mg/get-db conn "demo04")
         coll "user_profile"
         cr (mc/find db coll)]
     (response (mapv convert-to-map cr))
@@ -25,8 +26,7 @@
 
 
 (defn new-tag [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
+  (let [db (mg/get-db conn "demo04")
         coll "tags"]
     (try
       (prn (:body request))
@@ -38,17 +38,17 @@
 
 
 
+
+
 (defn all-tag [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
-        coll "tags"
-        cr (mc/find db coll)]
-    (response (mapv convert-to-map cr)))
+  (let [db (mg/get-db conn "demo04")
+        coll "tags"]
+    (response (mapv convert-to-map (mc/find db coll)))
+    )
   )
 
 (defn update-tag [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
+  (let [db (mg/get-db conn "demo04")
         coll "tags"
         id (get-in request [:params :id])
         name (get-in request [:body :name])]
@@ -64,8 +64,7 @@
 
 
 (defn batch-update [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
+  (let [db (mg/get-db conn "demo04")
         coll "user_profile"]
     (prn (:body request))
     (doseq [{:keys [id tags del]} (:body request)]
@@ -80,8 +79,7 @@
 
 
 (defn query [request]
-  (let [conn (mg/connect {:host "10.126.21.136"})
-        db (mg/get-db conn "demo04")
+  (let [db (mg/get-db conn "demo04")
         coll "user_profile"
         t (get-in request [:params :t] "")
         strs (->> (clojure.string/split t #",")
@@ -89,5 +87,10 @@
                   (filter #(< 1 (count %)))
                   )]
     (if (seq strs)
-      (response (mapv convert-to-map (mc/find db coll {:tags {$in strs}})))
+      (response {:count (mc/count db coll {:tags {$all strs}})
+                 :items (->>
+                          (with-collection db coll
+                                           (find {:tags {$all strs}})
+                                           (limit 50))
+                          (mapv convert-to-map))})
       (response []))))
